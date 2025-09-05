74485c80266d6e8735b8bd5e6aaf74124f01edf3
Yuxin Duan <hydrablaster@Duans-MacBook-Pro.local>
2025-09-05 11:28:04 -0500

    tests: skip napari UI test when napari not installed (importorskip)

--- tests/napari_curvealign_test.py
+++ tests/napari_curvealign_test.py
@@ -1,7 +1,14 @@
-import napari
-viewer = napari.Viewer()
-viewer.window.console
-# In console:
-from napari_plugin_engine import plugin_manager
-plugin_manager.discoverable_plugins
-# Should list 'napari-curvealign'
+# import napari
+# viewer = napari.Viewer()
+# viewer.window.console
+# # In console:
+# from napari_plugin_engine import plugin_manager
+# plugin_manager.discoverable_plugins
+# # Should list 'napari-curvealign'
+import pytest
+
+napari = pytest.importorskip("napari")
+
+def test_napari_available():
+    viewer = napari.Viewer()
+    assert viewer is not None
