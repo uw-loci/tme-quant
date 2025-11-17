d3f275ecc91d1e331f106cbb70bdb20f389e6fd3
Phillip Duan <yduan47@wisc.edu>
2025-11-17 15:57:14 -0600

    refactor: Remove MATLAB reference files from napari plugin
    Removed src/curvealign_matlab/ as the napari plugin uses the Python API (curvealign_py).
    MATLAB files are preserved in git history if needed for reference.

--- src/napari_curvealign/__init__.py
+++ src/napari_curvealign/__init__.py
@@ -2,7 +2,11 @@
 
 __version__ = "0.1.0"
 
-def napari_experimental_provide_dock_widget():
-    """Provide the CurveAlign widget to napari."""
+def napari_experimental_provide_dock_widget(viewer=None):
+    """Provide the CurveAlign widget to napari.
+    
+    For npe2 plugins, this function is called as a command and should
+    return a widget instance directly.
+    """
     from .widget import CurveAlignWidget
-    return [(CurveAlignWidget, {})]
+    return CurveAlignWidget(viewer=viewer)
